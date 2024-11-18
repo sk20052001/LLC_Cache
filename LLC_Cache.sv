d1a4a85449b5f3775d17502bc0fd0e72c5f66f3a
sk20052001 <sanjeev@pdx.edu>
2024-11-18 01:48:18 -0800

    cache implementation optimized

--- LLC_Cache.sv
+++ LLC_Cache.sv
@@ -11,27 +11,44 @@ module LLC(
     output logic [7:0] data_out
 );
 
+    logic [BYTE_OFFSET - 1:0] byte_offset;
+    logic [INDEX - 1:0] index;
+    logic [TAG_BITS - 1:0] tag;
+
+    assign tag <= addr[31:32-TAG_BITS];
+    assign index <= addr[31-TAG_BITS:BYTE_OFFSET];
+    assign byte_offset <= addr[BYTE_OFFSET-1:0];
+
     cache LLC_cache [NUM_SETS][ASSOCIATIVITY];
 
-    always @(posedge clk) begin
-        if (!reset) begin
-            LLC_cache <= '{default:0};
-        end else if (cs) begin
-            logic [BYTE_OFFSET - 1:0] byte_offset <= addr[5:0];
-            logic [INDEX - 1:0] index <= addr[19:6];
-            logic [TAGS - 1:0] tags <= addr[31:20];
-            for (int i = 0 ; i < ASSOCIATIVITY ; i++) begin
-                if (LLC_cache[index][i].tags == tags && LLC_cache[index][i].valid) begin
-                    hit <= 1'b1;
-                    if (wr)
-                        LLC_cache[index][i].data[byte_offset] <= data_in;
-                    else
-                        LLC_cache[index][i].data[byte_offset] <= LLC_cache[index][i].data[byte_offset];
-                        data_out <= LLC_cache[index][i].data[byte_offset];
-                end else begin
-                    hit = 1'b0;
-                    if (!wr)
+    logic [ASSOCIATIVITY - 2:0] p_lru [NUM_SETS];
+
+    logic [ASSOCIATIVITY - 1:0] hit_line;
+
+    always_ff @(posedge clk, posedge reset) begin
+        if (reset) begin
+            for (int i = 0; i < NUM_SETS; i++) begin
+                plru_tree[i] <= ASSOCIATIVITY - 1'b0;
+                for (int j = 0; j < ASSOCIATIVITY; j++) begin
+                    LLC_cache[i][j].valid <= 0;
+                    LLC_cache[i][j].dirty <= 0;
+                    LLC_cache[i][j].tag <= 0;
+                    for (int k = 0; k < LINE_SIZE; k++) begin
+                        LLC_cache[i][j].data[k] <= 0;
+                    end
                 end
+            end
+        end else begin
+            for (int i = 0; i < ASSOCIATIVITY; i++) begin
+                if (LLC_cache[index][i].valid && LLC_cache[index][i].tag == tag) begin
+                    hit_line[i] = 1'b1;
+                end
+            end
+            if (|hit_line) begin
+                if (wr)
+                    LLC_cache[index][$clog(hit_line)].data[byte_offset] <= data_in;
+                else
+                    data_out <= LLC_cache[index][$clog(hit_line)].data[byte_offset];
             end
         end
     end
